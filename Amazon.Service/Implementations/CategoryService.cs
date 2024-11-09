a3cd5e8f7827d44903eed78e7a9b49144605009b
Khaled tarek <khaledtarek543@gmail.com>
2024-11-09 14:44:06 +0200

    category service and Interface

---
+++ Amazon.Service/Implementations/CategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Core.Entities;
+using Amazon.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.Service.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<Category> create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Amazon.Service/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.Service.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<Category> create();
+    }
+}
